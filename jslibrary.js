0f3b0d3a5f1c1082ccb4cab9779cedff1e0d4cf2
Nick McCrory <n.mccrory@icloud.com>
2015-06-17 13:49:24 -0700

    first commit

---
+++ jslibrary.js
@@ -0,0 +1,9 @@
+var _ = {
+	map: function(){},
+	reduce: function(){},
+	find: function(){},
+	filter: function(){},
+	reject: function(){}
+}
+
+var evens = _.filter([1,2,3,4,5,6], function(num){return num % 2 == 0;});
