bd6f0ccf37c9b9ad22a95d63ba119dff04729c11
Nick McCrory <n.mccrory@icloud.com>
2015-06-17 15:25:03 -0700

    All methods are functional and working properly, project finished

--- jslibrary.js
+++ jslibrary.js
@@ -12,15 +12,47 @@ var _ = {
 		}
 		return result;
 	},
-	find: function(){},
-	filter: function(){},
-	reject: function(){}
+	find: function(list, locate){
+		var result = 0;
+		for(var i=0;list.length;i++){
+			if(locate(list[i])){
+				result = list[i];
+				break;
+			}
+		}
+		return result;
+	},
+	filter: function(list, filter){
+		var result = [];
+		for(var i=0;i<list.length;i++){
+			if(filter(list[i])){
+				result.push(list[i]);
+			}
+		}
+		return result;
+	},
+	reject: function(list,filter){
+		var result = [];
+		for(var i=0;i<list.length;i++){
+			if(!filter(list[i])){
+				result.push(list[i]);
+			}
+		}
+		return result;
+	}
 }
 
 var byThree = _.map([2,5,7], function(num){ return num*3;});
 console.log(byThree);
 
-var reduce = _.reduce([1,2,3], function(memo,num){ return memo+num;})
+var reduce = _.reduce([1,2,3], function(memo,num){ return memo+num;});
 console.log(reduce);
-//var evens = _.filter([1,2,3,4,5,6], function(num){return num % 2 == 0;});
-//console.log(evens);
+
+var even = _.find([1, 2, 3, 4, 5, 6], function(num){return num % 2 == 0;});
+console.log(even);
+
+var evens = _.filter([1,2,3,4,5,6], function(num){return num % 2 == 0;});
+console.log(evens);
+
+var odds = _.reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+console.log(odds);
