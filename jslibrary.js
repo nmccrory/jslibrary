55609337b067a9fdb16b2e1403e0d900048f8478
Nick McCrory <n.mccrory@icloud.com>
2015-06-17 14:28:10 -0700

    added functionality for map and reduce methods

--- jslibrary.js
+++ jslibrary.js
@@ -1,9 +1,26 @@
 var _ = {
-	map: function(){},
-	reduce: function(){},
+	map: function(list, iteratee){
+		for(var i=0;i<list.length;i++){
+			list[i] = iteratee(list[i]);
+		}
+		return list;
+	},
+	reduce: function(list, consolidate){
+		var result = 0;
+		for(var i=0;i<list.length;i++){
+			result = consolidate(result,list[i]);
+		}
+		return result;
+	},
 	find: function(){},
 	filter: function(){},
 	reject: function(){}
 }
 
-var evens = _.filter([1,2,3,4,5,6], function(num){return num % 2 == 0;});
+var byThree = _.map([2,5,7], function(num){ return num*3;});
+console.log(byThree);
+
+var reduce = _.reduce([1,2,3], function(memo,num){ return memo+num;})
+console.log(reduce);
+//var evens = _.filter([1,2,3,4,5,6], function(num){return num % 2 == 0;});
+//console.log(evens);
